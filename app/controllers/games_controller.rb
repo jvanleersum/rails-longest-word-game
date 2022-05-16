52552ab73b7f13e650e921faa41cb45acab81ad2
Judith van Leersum <judithvleersum@gmail.com>
2022-05-16 16:44:12 +0200

    Refactored code

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,13 +8,8 @@ class GamesController < ApplicationController
     9.times { |_| @letters << alphabet.sample(1)[0].upcase }
   end
 
-  def score
-    word = params[:word]
-    @word_upcase = word.split("").map {|w| w.upcase}.join()
-    @letters = params[:letters]
-    word_array = word.split("")
-    letters_array = @letters.split(" ").map { |l| l.downcase }
-    valid_array = word_array.map do |letter| 
+  def valid?(word_array, letters_array)
+    valid_array = word_array.map.all? do |letter| 
       if letters_array.include?(letter)
         index = letters_array.find_index(letter)
         letters_array.delete_at(index)
@@ -23,15 +18,22 @@ class GamesController < ApplicationController
         false
       end 
     end
-    @valid = valid_array.all? 
+  end
+
+  def found?(word)
+    response = JSON.parse(URI.open("https://wagon-dictionary.herokuapp.com/#{word}").read)
+    response["found"] == true
+  end
+
+  def score
+    word = params[:word]
+    @word_upcase = word.split("").map {|w| w.upcase}.join()
+    @letters = params[:letters]
+    letters_array = @letters.split(" ").map { |l| l.downcase }
+    @valid = valid?(word.split(""), letters_array)
     if @valid
-      response = JSON.parse(URI.open("https://wagon-dictionary.herokuapp.com/#{word}").read)
-      @found = response["found"] == true
-      if @found
-        @result = word.length * 13
-      else
-        @result = "not a valid English word"
-      end
+      @found = found?(word)
+      @result = @found ? word.length * 13 : "not a valid English word" 
     else
       @result = "not a valid word based on the given letters: "
     end
